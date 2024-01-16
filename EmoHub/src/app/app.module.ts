627c920ba5f8b28d7a0b88dc57a6136b8f675c49
Tshegofatso Motlhakane <tshegofatsomotlhakane@gmail.com>
2024-01-16 09:23:39 +0200

    add catalog component

--- EmoHub/src/app/app.module.ts
+++ EmoHub/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Component/home/home.component';
+import { CatalogComponent } from './Component/catalog/catalog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule,
