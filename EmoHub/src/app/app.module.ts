c2af4d725996597d25de16686b711f6aabcb2122
Tshegofatso Motlhakane <tshegofatsomotlhakane@gmail.com>
2024-04-30 14:47:14 +0200

    update

--- EmoHub/src/app/app.module.ts
+++ EmoHub/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Component/home/home.component';
-import { CatalogComponent } from './Component/catalog/catalog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CatalogComponent
   ],
   imports: [
     BrowserModule,
